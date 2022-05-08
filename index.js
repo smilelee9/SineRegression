998fd50b9a46ebbc0a51b9abeb00fc355f3d97a9
Leesoft <62933430+smilelee9@users.noreply.github.com>
2022-05-08 17:43:44 +0900

    2022.05.08 Vanila JS Version

---
+++ index.js
@@ -0,0 +1,41 @@
+function innerProduct(vec_a, vec_b) {
+    /*
+    Calculate inner product of two vecter, vec_a and vec_b.
+    */
+   let ret = 0
+   if (vec_a.length == vec_b.length) {
+        for (let i=0; i<vec_a.length; i++) {
+            ret += vec_a[i] * vec_b[i];
+        }
+   }
+   return ret;
+}
+
+function regression(x, y) {
+    /*
+    Curve fitting of form y=a*sin(x+b), where a is positive and b is in [0, 2*pi).
+    It uses angle addition formula and bilinear least-square method.
+
+    Let A = a cos(b), B = a sin(b), Si = sin(xi) and Ci = cos(xi)
+    y = a sin(x+b) = a cos(b)sin(x) +  sin(b)cos(x) = A*Si + B*Ci.
+    Here, the bilinear model y = f(S,C) = AS + BC can be obtained.
+    */
+    const [S, C] = [x.map(Math.sin), x.map(Math.cos)];
+    
+    const d = innerProduct(S, S) * innerProduct(C, C) - innerProduct(S, C) * innerProduct(S, C);
+    const A = (innerProduct(y, S) * innerProduct(C, C) - innerProduct(S, C) * innerProduct(y, C)) / d;
+    const B = (innerProduct(S, S) * innerProduct(y, C) - innerProduct(y, S) * innerProduct(S, C)) / d;
+    let [a, b] = [Math.sqrt(A**2 + B**2), Math.atan(B/A)];
+    let [posErr, negErr] = [0, 0];
+    for (let i=0; i<x.length; i++) {
+        posErr += (y[i] - a * Math.sin(x[i]+b)) ** 2;
+        negErr += (y[i] + a * Math.sin(x[i]+b)) ** 2;
+    }
+    a = posErr <= negErr ? a : -a;
+    return [a, b];
+}
+
+x = [0.6529102266243976, 0.961385572507321, 1.8980272152216007, 3.073049109742477, 3.963094970855199];
+y = [0.01455087912955555, 1.6379700510986985, 4.676519019633776, 3.265370823709192, -0.9482382666959175];
+
+console.log(regression(x, y));
